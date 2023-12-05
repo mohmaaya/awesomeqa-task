461bccc49c9ed381b0c1ece1ba65c7b559337123
aj79ysid <mohammed.ughratdar@fau.de>
2023-12-05 14:45:50 +0100

    Added the REST methods of GET and PUT for fetching and updating the ticket

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from app.repositories.ticket_repository import TicketRepository
 import uvicorn
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
 
 
@@ -14,14 +14,33 @@ ticket_repository = TicketRepository(filepath=TICKET_FILEPATH)
 async def root():
     return "OK"
 
-
 @app.get("/tickets")
 async def get_tickets(
-    limit: int = 20,
     ticket_repository: TicketRepository = Depends(lambda: ticket_repository),
 ):
-    tickets = ticket_repository.get_tickets(limit)
+    tickets = ticket_repository.get_tickets()
     return JSONResponse(tickets, status_code=200)
+
+@app.put("/ticket/{id}")
+async def update_ticket_status(
+    id: str,
+    request: Request,
+    ticket_repository: TicketRepository = Depends(lambda: ticket_repository),
+):
+
+    request_body = await request.json()
+    status = request_body.get("status")
+
+    if status is None:
+        raise HTTPException(status_code=400, error="Status is not provided")
+
+    updated_ticket_status = ticket_repository.update_ticket_status(id, status)
+
+    if updated_ticket_status:
+        tickets = ticket_repository.get_tickets()
+        return JSONResponse(tickets, status_code=200)
+   
+    raise HTTPException(status_code=400, error="Update failed. Ticket not found")
 
 
 if __name__ == "__main__":
--- backend/app/repositories/ticket_repository.py
+++ backend/app/repositories/ticket_repository.py
@@ -8,4 +8,23 @@ class TicketRepository:
             self.data = json.load(json_file)
 
     def get_tickets(self, limit: Optional[int] = None) -> list[dict]:
-        return self.data["tickets"][:limit]
+        return  [
+    {
+        "id": ticket["id"],
+        "status": ticket["status"],
+        "timestamp": ticket["timestamp"],
+        "message_url": message["msg_url"],
+        "content": message["content"],
+        "author_name": message["author"]["name"]
+    }
+    for ticket in self.data["tickets"]
+    for message in self.data["messages"]
+    if ticket["msg_id"] == message["id"]
+    ]
+
+    def update_ticket_status(self, id: str, status:str) -> bool:
+         for ticket in self.data["tickets"]:
+            if ticket["id"] == id:  
+                ticket["status"] = status
+                return True
+         return False 
